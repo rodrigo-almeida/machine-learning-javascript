14c8bd8808d97062aa44867b396061715e75f31a
Rodrigo Almeida <rodrigoalmeida.contato@gmail.com>
2019-02-09 12:53:36 -0200

    Team matches with brain.js

--- main.js
+++ main.js
@@ -12,21 +12,25 @@ const brain = require('brain.js');
  *      1: Second team wins
  */
 function teamMatches() {
-    const network = new brain.NeuralNetwork();
+    const 
+        network = new brain.NeuralNetwork(),
+        samples = [
+            {input: [1,2], output: [0]}, // Team 1 wins
+            {input: [1,3], output: [1]}, // Team 3 wins
+            {input: [1,4], output: [1]}, // Team 4 wins
+            {input: [2,3], output: [0]}, // Team 2 wins
+            {input: [2,4], output: [1]}, // Team 4 wins
+        ],
+        input = [2,3]; 
 
-    network.train([
-        {input: [1,2], output: [0]},
-        {input: [1,3], output: [1]},
-        {input: [1,4], output: [1]},
-        {input: [2,3], output: [0]},
-        {input: [2,4], output: [1]},
-    ]);
-
-    const input = [2,3];
-    
+    network.train(samples);
     const output = network.run(input);
     
-    console.log(`Team ${input[1]} prob: ${output}`);
+    return `Team ${input[1]} prob: ${output}`;
 }
 
-teamMatches();
+// Model to run
+const modelToRun = teamMatches; // Change this to the desired model
+
+// Output log
+console.log(modelToRun());
